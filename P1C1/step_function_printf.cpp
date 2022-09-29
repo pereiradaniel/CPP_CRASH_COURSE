1ef7366cb2a3b2ee4cad7eacf7363962c93f2c76
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-09-29 15:56:25 -0400

    Use printf instead of cout.

---
+++ P1C1/step_function_printf.cpp
@@ -0,0 +1,23 @@
+// printf function is a derivative of the writef function from BCPL.
+// Implement step_function.cpp using printf
+#include <cstdio>
+
+int step_function(int x)
+{
+    int result = 0;
+ 
+    if (x<0)
+        result = -1; 
+    else if (x>0)
+        result = 1;
+
+    return result;
+}
+
+int main()
+{
+    int value1 = step_function(100);  // value1 =  1
+    int value2 = step_function(0);    // value2 =  0
+    int value3 = step_function(-100); // value3 = -1
+    printf("value1: %d\nvalue2: %d\nvalue3: %d\n", value1, value2, value3);
+}
