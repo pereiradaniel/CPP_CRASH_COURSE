efd3a04f7d78a133ba659556e2a96e0b5897c691
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-02-27 15:12:35 -0500

    Added copy_semantics.cpp to p1c4 and some notes on copy semantics.

---
+++ P1C4/copy_semantics.cpp
@@ -0,0 +1,19 @@
+// Demonstrates an aspect of copy semantics.
+
+#include <cstdio>
+
+int add_one_to(int x) {
+    // add_one_to takes its argument x by value. Then it modifies the value of x.
+    // This modification is isolated from the calling function!
+    ++x;
+    return x;
+}
+
+int main() {
+    auto original = 1;
+    
+    // original will be unaffected by the function call, because add_one_to receives a copy of original.
+    auto result = add_one_to(original);
+    
+    printf("Original: %d; Result:%d", original, result); 
+}
