c67389510a76c22a0119b395330d7c1ff5bb2f47
Daniel Pereira <danielpaulpereira@protonmail.com>
2025-03-12 14:10:42 -0400

    Added rvalue and lvalue notes to rvalue_lvalue.cpp

--- P1C4/rvalue_lvalue.cpp
+++ P1C4/rvalue_lvalue.cpp
@@ -11,7 +11,13 @@ void ref_type(int &&x) {
 
 int main() {
     auto x = 1;
+
+    // x is an lvalue because it has a name:
     ref_type(x);
+
+    // 2 is an rvalue because it is an integer literal without a name:
     ref_type(2);
+
+    // The result of adding x and 2 is not bound to a name, so it's an rvalue:
     ref_type(x+2);
 }
