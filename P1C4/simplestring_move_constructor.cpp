fbfb8246a47057261f0fbef3a7ad8ebce549c6de
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-03-14 18:58:18 -0400

    Added move assignment to simplestring_move_constructor.cpp but not yet tested or implemented.

--- P1C4/simplestring_move_constructor.cpp
+++ P1C4/simplestring_move_constructor.cpp
@@ -22,6 +22,19 @@ struct SimpleString {
         other.max_size = 0;
     }
 
+    // Move assignment:
+    SimpleString& operator=(SimpleString&& other) noexcept {
+        if (this == &other) return *this;
+        delete[] buffer;
+        buffer = other.buffer;
+        length = other.length;
+        max_size = other.max_size;
+        other.buffer = nullptr;
+        other.length = 0;
+        other.max_size = 0;
+        return *this;
+    }
+
     // Copy Constructor
     // Use member initializers for max_size, buffer, and length:
     // Use array new to initialize buffer:
