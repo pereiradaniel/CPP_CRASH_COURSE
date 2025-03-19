43e8d2ce724ce97fd5fccbc3364ff220a5c23b60
Daniel Pereira <danielpaulpereira@protonmail.com>
2025-03-19 17:57:21 -0400

    Add skeleton for simplestring_copy_move_complete.cpp

--- P1C4/simplestring_copy_move_complete.cpp
+++ P1C4/simplestring_copy_move_complete.cpp
@@ -1,2 +1,34 @@
 // SimpleString Final Product
 
+#include <cstdio>
+#include <cstring>
+#include <stdexcept>
+#include <utility>
+
+struct SimpleString {
+    SimpleString(size_t max_size) {}
+
+    ~SimpleString() {}
+
+    SimpleString(const SimpleString& other) {}
+
+    SimpleString(SimpleString&& other) noexcept :
+        max_size(other.max_size),
+        buffer(other.buffer),
+        length(other.length) {
+
+        }
+
+    SimpleString& operator=(const SimpleString& other) {}
+
+    SimpleString& operator=(SimpleString&& other) noexcept {}
+
+    void print(const char* tag) const {}
+
+    bool append_line(const char* x) {}
+
+    private:
+        size_t max_size;
+        char* buffer;
+        size_t length;
+};
