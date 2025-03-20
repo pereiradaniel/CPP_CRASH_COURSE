35d5ac478812b434984860d5e1c12925656dd90d
Daniel Pereira <danielpaulpereira@protonmail.com>
2025-03-20 16:05:12 -0400

    Added pring and append_line with comments.

--- P1C4/simplestring_copy_move_complete.cpp
+++ P1C4/simplestring_copy_move_complete.cpp
@@ -102,10 +102,32 @@ struct SimpleString {
         return *this;
     }
 
-    void print(const char* tag) const {}
+    // Prints the string. const used because there is no need to modify the state of the SimpleString:
+    void print(const char* tag) const {
+        printf("%s: %s", tag, buffer);
+    }
 
-    bool append_line(const char* x) {}
+    // This method takes a null-terminated string x and adds its contents plus a newline char to buffer:
+    bool append_line(const char* x) {
 
+        // Determine the length of x using strlen() from <cstring>:
+        const auto x_len = strlen(x);
+
+        // Determine whether appending x (a newline char) and a null byte to the current string would result in a string with length greater than max_size, return false if it does:
+        if (x_len + length + 2 > max_size) return false;
+
+        // If there is enough room to append x, copy its bytes into the correct location in nbuffer:
+        std::strncpy(buffer+length,x,max_size-length);
+        length += x_len;
+
+        // After adding the number of bytes x_len coped into buffer to length, finish by adding a newline char \n and a null byte to the end of the buffer.
+        buffer[length++]='\n';
+        buffer[length] = 0;
+
+        // Return true to indicate successfully appended x as a line to the end of buffer:
+        return true;
+    }
+    
     private:
         size_t max_size;
         char* buffer;
