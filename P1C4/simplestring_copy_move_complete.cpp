18cea898ff298e62702297daa882738e9a1871b3
Daniel Pereira <danielpaulpereira@protonmail.com>
2025-03-20 15:53:57 -0400

    Added code block to single argument constructor.

--- P1C4/simplestring_copy_move_complete.cpp
+++ P1C4/simplestring_copy_move_complete.cpp
@@ -6,9 +6,19 @@
 #include <utility>
 
 struct SimpleString {
-    SimpleString(size_t max_size) {}
+    SimpleString(size_t max_size) noexcept :
+    max_size(max_size),
+    length{} {
+        if (max_size == 0) {
+            throw std::runtime_error{"Max size must be at least 1."};
+        }
+        buffer = new char[max_size];
+        buffer[0] = 0;
+    }
 
-    ~SimpleString() {}
+    ~SimpleString() {
+        delete[] buffer;
+    }
 
     SimpleString(const SimpleString& other) {}
 
