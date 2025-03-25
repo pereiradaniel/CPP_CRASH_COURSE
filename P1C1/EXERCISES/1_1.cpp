3892367505b8af0bd99cf952a3daaf1012ecbcf0
Daniel Pereira <danielpaulpereira@protonmail.com>
2025-03-25 10:50:18 -0400

    Ran program 1_1 with another absolute value.

--- P1C1/EXERCISES/1_1.cpp
+++ P1C1/EXERCISES/1_1.cpp
@@ -18,4 +18,7 @@ int absolute_value(const int x) noexcept {
 int main() {
     int my_num = -10;
     printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+
+    int my_num2 = -50;
+    printf("The absolute value of %d is %d.\n", my_num2, absolute_value(my_num2));
 }
