ef964dbb1e2f61c3355712ac4435a58d8fabf429
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-03-25 10:48:48 -0400

    Added EXERCISES to P1C1, did first exercise and output valgrind results to a text file.

---
+++ P1C1/EXERCISES/1_1.cpp
@@ -0,0 +1,21 @@
+// Create a function called absolute_value that returns the absolute value of its single argument. The absolute value of an integer x is the following: x (itself) if x is greater than or equal to 0; otherwise, it is x times -1. You can use the program in listing 1-9 as a template:
+
+#include <cstdio>
+
+int absolute_value(const int x) noexcept {
+    // Create variable for result:
+    int result = 0;
+    
+    if (x >= 0)
+        result = x;
+    else if (x < 0)
+        result = x*(-1);
+    
+    // Return the result:
+    return result;
+}
+
+int main() {
+    int my_num = -10;
+    printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+}
