bf327e97acf2265ae0d756a0397b3ec51214566d
Daniel Pereira <danielpaulpereira@protonmail.com>
2025-03-25 10:53:19 -0400

    Added exercise 1_2.cpp, removed 1_2 from 1_1.cpp. Output results of both into separate txt files.

--- P1C1/EXERCISES/1_2.cpp
+++ P1C1/EXERCISES/1_2.cpp
@@ -18,4 +18,11 @@ int absolute_value(const int x) noexcept {
 int main() {
     int my_num = -10;
     printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+
+    // Exercise 1-2: Try running your program with differente values. Did you see what you expect?
+    int my_num2 = -50;
+    printf("The absolute value of %d is %d.\n", my_num2, absolute_value(my_num2));
+
+    int my_num3 = 75;
+    printf("The absolute value of %d is %d.\n", my_num3, absolute_value(my_num3));
 }
