d86b3b5a8e9b95a94b07a789458b4e842b1413fc
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-03-27 11:34:46 -0400

    Exercise 3_2.cpp tested and works correctly. Valgrind output added to text file.

--- P1C3/EXERCISES/3_2.cpp
+++ P1C3/EXERCISES/3_2.cpp
@@ -14,23 +14,23 @@ void write_to(char* array, const int index, const char character) {
     // Check bounds
     if (index <= (sizeof(array) / sizeof(char)) && index >= 0) {
         array[index] = character;
-        printf("\nArray after modification: %s\n", array);
+        printf("Array after modification: %s\n", array);
     }
     else printf("\nOut of bounds!\n");
 }
 
 int main() {
-  char lower[] = "abc?e";
-  char upper[] = "ABC?E";
-  char* upper_ptr = upper; // Equivalent: &upper[0]
+    char lower[] = "abc?e";
+    char upper[] = "ABC?E";
+    char* upper_ptr = upper; // Equivalent: &upper[0]
 
-  lower[3] = 'd'; // lower now contains a b c d e \0
-  upper_ptr[3] = 'D'; // upper now contains A B C D E \0
+    lower[3] = 'd'; // lower now contains a b c d e \0
+    upper_ptr[3] = 'D'; // upper now contains A B C D E \0
 
-  char letter_d = lower[3]; // letter_d equals 'd'
-  char letter_D = upper_ptr[3]; // letter_D equals 'D'
+    char letter_d = lower[3]; // letter_d equals 'd'
+    char letter_D = upper_ptr[3]; // letter_D equals 'D'
 
-  printf("lower: %s\nupper: %s", lower, upper);
+    printf("lower: %s\nupper: %s", lower, upper);
 
     read_from(lower, 3);
     write_to(upper, 2, 'X');
