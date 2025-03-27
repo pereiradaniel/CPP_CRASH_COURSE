1db15b62d182ce349584853dc12ac10261f5a671
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-03-27 11:57:09 -0400

    Added out_of_bounds() function to avoid duplicate printf() statements.

--- P1C3/EXERCISES/3_2.cpp
+++ P1C3/EXERCISES/3_2.cpp
@@ -1,17 +1,20 @@
 // Exercise 3-2: Add a read_from and a write_to function to Listing 3-6. These functions should read or write to upper or lower as appropriate. Perform bounds checking to prevent buffer overflows.
 
 #include <cstdio>
+void out_of_bounds() noexcept {
+    printf("Out of bounds!\n");
+}
 
 void read_from(const char* array, const int &index) noexcept {
     if (index <= (sizeof(array) / sizeof(char)) && index >= 0)
         printf("Char at index %d is %c.\n", index, array[index]);
-    else printf("Out of bounds!\n");
+    else out_of_bounds();
 }
 
 void write_to(char* array, const int &index, const char &character) noexcept {
     if (index <= (sizeof(array) / sizeof(char)) && index >= 0)
         array[index] = character;
-    else printf("Out of bounds!\n");
+    else out_of_bounds();
 }
 
 int main() {
