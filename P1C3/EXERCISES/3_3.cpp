91aea1ec10231351ab19b1d5271f26053790a2fe
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-03-28 11:00:02 -0400

    Added Exercise 3-3 instructions to 3_3.cpp

--- P1C3/EXERCISES/3_3.cpp
+++ P1C3/EXERCISES/3_3.cpp
@@ -1,4 +1,8 @@
 // Exercise 3-3:
+// Add an Element* previous to Listing 3-9 to make a doubly linked list.
+// Add an insert_before method to Element.
+// Traverse the list from front to back, then from back to front, using two separate for loops.
+// Print the operating_number inside each loop.
 
 struct Element {
     Element* next{};
