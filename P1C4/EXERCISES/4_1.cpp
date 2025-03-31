48c5d56d24e46a99cb24cd619519d5a2a4a20c1c
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-03-31 10:08:33 -0400

    Exercise 4_1.cpp added.

--- P1C4/EXERCISES/4_1.cpp
+++ P1C4/EXERCISES/4_1.cpp
@@ -3,3 +3,18 @@
 // In its constructor, record the current time in a field called timestamp.
 // (Compare wit hthe POSIX function gettimeofday).
 
+#include <chrono>
+struct TimerClass {
+    // Constructor:
+    TimerClass(const char* name)
+    :   timestamp{ std::chrono::system_clock::now() },
+        name{name}
+        {}
+    
+    private:
+    std::chrono::time_point<std::chrono::system_clock> timestamp;
+    const char* name;
+};
+
+int main() {
+}
