1d05ec6eb4e191abe15d30393751976841b20c17
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-03-31 10:21:16 -0400

    Exercise 4_2.cpp added.

--- P1C4/EXERCISES/4_1.cpp
+++ P1C4/EXERCISES/4_1.cpp
@@ -1,7 +1,7 @@
 // Exercise 4-1:
 // Create a struct TimerClass.
 // In its constructor, record the current time in a field called timestamp.
-// (Compare wit hthe POSIX function gettimeofday).
+// (Compare with the POSIX function gettimeofday).
 
 #include <chrono>
 struct TimerClass {
@@ -9,10 +9,10 @@ struct TimerClass {
     TimerClass(const char* name)
     :   timestamp{ std::chrono::system_clock::now() },
         name{name}
-        {}
+    {}
     
     private:
-    std::chrono::time_point<std::chrono::system_clock> timestamp;
+    std::chrono::time_point<std::chrono::system_clock> timestamp;   // Records the time of construction.
     const char* name;
 };
 
---
+++ P1C4/EXERCISES/4_2.cpp
@@ -0,0 +1,34 @@
+// Exercise 4-2:
+// In the destructor of TimerClass, record the current time and subtract the time at construction.
+// This time is rougly the age of the timer.
+// Print this value.
+#include <cstdio>
+#include <chrono>
+
+struct TimerClass {
+    // Constructor:
+    TimerClass(const char* name)
+    :   timestamp{ std::chrono::system_clock::now() },
+        name{name}
+    {}
+
+    // Destructor: 
+    ~TimerClass()
+    {
+        // Record the current time:
+        auto currentTime = std::chrono::system_clock::now();
+
+        // Subtract the time at construction from the curret time:
+        std::chrono::duration<double> elapsed_time = currentTime - timestamp;
+        
+        // Print the value containing the age of the timer:
+        printf("Age of the timer: '%s': %f\n", name, elapsed_time.count());
+    }
+
+    private:
+    std::chrono::time_point<std::chrono::system_clock> timestamp;   // Records the time of construction.
+    const char* name;
+};
+
+int main() {
+}
