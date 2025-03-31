6e56c5fd244e0a27e3fe5b71565b6c2529ca6c61
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-03-31 09:10:03 -0400

    Added Listing_3-11.cpp

---
+++ P1C3/EXERCISES/Listing_3-11.cpp
@@ -0,0 +1,18 @@
+// Listing 3-11
+#include <cstdio>
+
+int main() {
+    int original = 100;
+    int& original_ref = original;
+    
+    printf("Original: %d\n", original);         // Print the value of original.
+    printf("Reference: %d\n", original_ref);    // Print valed referred to by original, they are the same.
+
+    int new_value = 200;                        // Initialize another int.
+    // Assign original_ref to new_value, it assigns the value of new_value to the object it points to.
+    original_ref = new_value;
+
+    printf("Original: %d\n", original);         // 200
+    printf("New Value: %d\n", new_value);       // 200
+    printf("Reference: %d\n", original_ref);    // 200
+}
