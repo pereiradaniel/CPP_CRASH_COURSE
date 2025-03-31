6fe3e1dc3841f94fc02cdbbcb92e26904ba8c37b
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-03-31 09:39:07 -0400

    Fix typos in comments to 3-4.

--- P1C3/EXERCISES/3_4.cpp
+++ P1C3/EXERCISES/3_4.cpp
@@ -1,4 +1,4 @@
-// Exercise 3-4: Reimplement Listing 3-11 using no explicit types. (Hint: Use auto.)
+// Exercise 3-4: Reimplement Listing 3-11 using non explicit types. (Hint: Use auto.)
 #include <cstdio>
 
 int main() {
