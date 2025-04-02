9215e13e6ac369b856d21aa7cb381929aaba05cb
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-04-02 12:28:56 -0400

    Added comments to 5_2.cpp from the book.

--- P1C5/5_2.cpp
+++ P1C5/5_2.cpp
@@ -5,6 +5,7 @@
 #include <stdexcept>
 
 struct FileLogger {
+  // 1 Notionally add the ability to log to a file by implementing a FileLogger:
   void log_transfer(long from, long to, double amount) {
     //
     printf("[file] %ld,%ld,%f\n", from, to, amount);
@@ -17,17 +18,21 @@ struct ConsoleLogger {
   }
 };
 
+// 2 Create an enum class LoggerType so you can switch logging behaviour at runtime:
 enum class LoggerType { Console, File };
 
 struct Bank {
+  // 3 Initialize thje type field to Console within the Bank constructor:
   Bank()
       : type{ LoggerType::Console } {}
+  // 4 Add a set_logger function to perform desired logging behaviour:
   void set_logger(LoggerType new_type) {
     type = new_type;
   }
 
   void make_transfer(long from, long to, double amount) {
     //
+    // 5 Ise the type within make_transfer to switch on the correct logger:
     switch(type) {
     case LoggerType::Console: {
       consoleLogger.log_transfer(from, to, amount);
@@ -53,6 +58,7 @@ int main() {
   Bank bank;
   bank.make_transfer(1000, 2000, 49.95);
   bank.make_transfer(2000, 4000, 20.00);
+  // 6 Alter a Bank class' logging behaviour by uing the set_logger method, the object handles dispatching internally:
   bank.set_logger(LoggerType::File);
   bank.make_transfer(3000, 2000, 75.00);
 }
