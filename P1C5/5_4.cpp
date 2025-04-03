26f1183cf5a9f5df2d08e4e77413a9d325e70cc7
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-04-03 10:42:34 -0400

    Added Listing 5-4 with notes to 5_4.cpp

---
+++ P1C5/5_4.cpp
@@ -0,0 +1,10 @@
+// Listing 5-4:
+
+struct BaseClass {};
+struct DerivedClass : BaseClass{};          // DerivedClass derives from BaseClass.
+void are_belong_to_us(BaseClass& base) {}   // Takes reference to BaseClass argumnt 'base'.
+
+int main() {
+    DerivedClass derived;
+    are_belong_to(derived);                 // Can be invoked with an instance of Derived class. 
+}
