11cf9198622e4c6e46ad364aa63c7e9851d40f2d
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-04-03 10:47:11 -0400

    Added Listing 5-5.

---
+++ P1C5/5_5.cpp
@@ -0,0 +1,11 @@
+// Listing 5-5:
+#include <cstdio>
+
+struct BaseClass {};
+struct DerivedClass : BaseClass {};
+void all_about_that(DerivedClass& derived) {}
+
+int main() {
+    BaseClass base;
+    //all_about_that(base); // No! Trouble!
+}
