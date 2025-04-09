eb62d6ee0c72755c3b912e42185b109c80ad7640
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-04-09 10:04:22 -0400

    Added 5_8.cpp

---
+++ P1C5/5_8.cpp
@@ -0,0 +1,20 @@
+// Listing 5-8: A refactor of 5-7 using a pure virtual method.
+#include <cstdio>
+
+struct BaseClass {
+  virtual const char* final_message() const = 0;
+};
+
+struct DerivedClass : BaseClass {
+  const char* final_message() const override {
+    return "We apologise for the inconvenience.";
+  }
+};
+
+int main() {
+  // BaseClass base; // Bang!
+  DerivedClass derived;
+  BaseClass& ref = derived;
+  printf("DerivedClass: %s\n", derived.final_message());
+  printf("BaseClass&:   %s\n", ref.final_message());
+}
