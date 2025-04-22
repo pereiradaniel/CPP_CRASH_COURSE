2697a7cc7c04a4f154d0a76811572ebc405e1949
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-04-22 05:38:05 -0400

    Added comments to 5-10.

--- P1C5/5_10.cpp
+++ P1C5/5_10.cpp
@@ -1,9 +1,10 @@
-//  Listing 5-10:
+//  Listing 5-10: A refactor of Listing 5-9 with a virtual destructor.
 
 #include <cstdio>
 
 struct BaseClass {
-  virtual ~BaseClass() = default;
+    // Adding the virtual destructor causes the DerivedClass destructor to get invoked when you delete the BaseClass pointer:
+    virtual ~BaseClass() = default;
 };
 
 struct DerivedClass : BaseClass {
@@ -11,13 +12,15 @@ struct DerivedClass : BaseClass {
     printf("DerivedClass() invoked.\n");
   }
   ~DerivedClass() {
+    // Derived Class destructor prints this message when invoked:
     printf("~DerivedClass() invoked.\n");
   }
 };
 
 int main() {
-  printf("Constructing DerivedClass x.\n");
-  BaseClass* x{ new DerivedClass{} };
-  printf("Deleting x as a BaseClass*.\n");
-  delete x;
+    printf("Constructing DerivedClass x.\n");
+    BaseClass* x{ new DerivedClass{} };
+    printf("Deleting x as a BaseClass*.\n");
+    // Delete the BaseClass pointer.
+    delete x;
 }
