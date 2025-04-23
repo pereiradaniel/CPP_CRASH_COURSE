01fc4f274be29bff830d3b04560a4f75aceff586
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-04-23 12:43:04 -0400

    Added comments to 5-11.

--- P1C5/5_11.cpp
+++ P1C5/5_11.cpp
@@ -2,27 +2,32 @@
 #include <cstdio>
 
 struct Logger {
-  virtual ~Logger() = default;
-  virtual void log_transfer(long from, long to, double amount) = 0;
+    // Logger is a pure virtual class (interface) with a default virtual destructor:
+    virtual ~Logger() = default;
+    virtual void log_transfer(long from, long to, double amount) = 0;
 };
 
+// ConsoleLogger are Logger implementations because they derive from the interface:
 struct ConsoleLogger : Logger {
-  void log_transfer(long from, long to, double amount) override {
-    printf("[cons] %ld -> %ld: %f\n", from, to, amount);
-  }
+    // log_transfer implemented and the override keyword placed:
+    void log_transfer(long from, long to, double amount) override {
+        printf("[cons] %ld -> %ld: %f\n", from, to, amount);
+    }
 };
 
+// FileLogger are Logger implementations because they derive from the interface:
 struct FileLogger : Logger {
-  void log_transfer(long from, long to, double amount) override {
-    printf("[file] %ld,%ld,%f\n", from, to, amount);
-  }
+    // log_transfer implemented and the override keyword placed:
+    void log_transfer(long from, long to, double amount) override {
+        printf("[file] %ld,%ld,%f\n", from, to, amount);
+    }
 };
 
 struct Bank {
   Bank(Logger& logger)
       : logger{ logger } {}
   void make_transfer(long from, long to, double amount) {
-    //
+    // log_transfer implemented and the override keyword placed:
     logger.log_transfer(from, to, amount);
   }
 
