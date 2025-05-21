59fe01ec30183cfd57540404a93366167447d361
Daniel Pereira <danielpaulpereira@protonmail.com>
2025-05-21 15:10:46 -0400

    Fixed typos in 5_5.cpp and added additional notation about how the program will not compile if line 9 is uncommented (base class trying to invoke inheritance from derived class).

--- P1C5/EXAMPLES/5_5.cpp
+++ P1C5/EXAMPLES/5_5.cpp
@@ -1,7 +1,7 @@
-// Listing 5-5: This program attempts to use a base classin place of a derived class. (This listing won't compile.)
+// Listing 5-5: This program attempts to use a base class in place of a derived class. (This listing won't compile when line 9 is uncommented.)
 
 struct BaseClass {};
-struct DerivedClass : BaseClass {};             // BaseClass does't derive from DerivedClass.
+struct DerivedClass : BaseClass {};             // BaseClass doesn't derive from DerivedClass.
 void all_about_that(DerivedClass& derived) {}   // Function taks DerivedClass argument.
 
 int main() {
