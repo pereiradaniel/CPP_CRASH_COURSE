3a5ea8b78c00af15e2791459bf53055584b03065
pereiradaniel <danielpaulpereira@protonmail.com>
2026-01-08 10:29:29 -0500

    main() added to 5-4.cpp to test code.

--- P1C5/EXERCISES/5_4.cpp
+++ P1C5/EXERCISES/5_4.cpp
@@ -1,6 +1,7 @@
 // 5-4.    Modify ConsoleLogger to accept a const char* at construction. When ConsoleLogger logs, prepend this string to the logging output. Notice that you can modify logging behaviour without having to modify Bank.
 
 #include <cstdio>
+#include <exception>
 
 class Logger {
 public:
@@ -32,15 +33,16 @@ public:
 };
 
 class AccountDatabase {
+public:
     virtual ~AccountDatabase() = default;
     virtual void setAmount(long from, long to, double amount) = 0;
 };
 
-class InMemoryAccountDatabase : AccountDatabase {
-    public:
-        void setAmount(long from, long to, double amount) override {
-            printf("%ld -> %ld: %.2f\n", from, to, amount);
-        }
+class InMemoryAccountDatabase : public AccountDatabase {
+public:
+    void setAmount(long from, long to, double amount) override {
+        printf("%ld -> %ld: %.2f\n", from, to, amount);
+    }
 };
 class Bank {
 public:
@@ -57,7 +59,35 @@ public:
         if (logger)
             logger->log_transfer(from, to, amount);
     }
+
 private:
     Logger *logger;
     InMemoryAccountDatabase &accountDB;
 };
+
+int main() {
+    try {
+        ConsoleLogger console_logger("prefix: ");
+        FileLogger file_logger;
+        InMemoryAccountDatabase bankAccounts;
+        Bank bank { bankAccounts};
+
+        bank.set_logger(&console_logger);
+        bank.make_transfer(100,200,300.55);
+
+        printf("\n");
+
+        bank.set_logger(&file_logger);
+        bank.make_transfer(400,500,600.77);
+    }
+    catch(const std::exception& e) {
+        printf("Exception: %s\n", e.what());
+        return 1;
+    }
+    catch(...) {
+        printf("Unknown exception!\n");
+        return 2;
+    }
+
+    return 0;
+}
