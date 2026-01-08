7497a62e5a7f880f1d6e1a0498412f3eff2aba55
pereiradaniel <danielpaulpereira@protonmail.com>
2026-01-08 09:37:05 -0500

    Exercise 5-1 added.

---
+++ P1C5/EXERCISES/5_1.cpp
@@ -0,0 +1,6 @@
+// 5-1.    You didn't implement an accounting system in your Bank. Design an interface called AccountDatabase that can retrieve and set amounts in bank accounts (identified by a long id).
+
+class AccountDatabase {
+    virtual ~AccountDatabase() = default;
+    virtual void setAmount(long from, long, to, double amount) = 0;
+};
