42d2d19d5374c0f6bf51d669471e30e12512ab45
pereiradaniel <danielpaulpereira@protonmail.com>
2026-01-08 09:45:10 -0500

    Added 5-2.

---
+++ P1C5/EXERCISES/5_2.cpp
@@ -0,0 +1,14 @@
+// 5-2.    Genereate an InMemoryAccountDatabase that implements AccountDatabase.
+
+#include <cstdio>
+class AccountDatabase {
+    virtual ~AccountDatabase() = default;
+    virtual void setAmount(long from, long to, double amount) = 0;
+};
+
+class InMemoryAccountDatabase : AccountDatabase {
+    public:
+        void setAmount(long from, long to, double amount) override {
+            printf("%ld -> %ld: %.2f\n", from, to, amount);
+        }
+};
