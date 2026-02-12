c76f685b2c06763a2ec0d61d2451541fda349b34
Daniel Pereira <danielpaulpereira@protonmail.com>
2026-02-12 16:26:05 -0500

    Updated notes up until Named Conversion Functions.

--- P1C6/EXAMPLES/6_2.cpp
+++ P1C6/EXAMPLES/6_2.cpp
@@ -4,6 +4,8 @@
 #include <cstdio>
 
 template<typename X, typename Y, typename Z>
+// Template function that takes three template parameters: X, Y, and Z.
+// It takes a reference to a Y and a pointer to a const Z, and returns an X.
 X my_template_function(Y& arg1, const Z* arg2) {
     // Example implementation: just prints the arguments and returns a default value of type X
     std::printf("arg1: %f, arg2: %c\n", static_cast<double>(arg1), *arg2);
