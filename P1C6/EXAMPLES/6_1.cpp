71fa5ddce65720f118ecf73c59281e3fc3a47f3d
Daniel Pereira <danielpaulpereira@protonmail.com>
2026-02-12 16:07:24 -0500

    Updated notes for template class definitions.

--- P1C6/EXAMPLES/6_1.cpp
+++ P1C6/EXAMPLES/6_1.cpp
@@ -3,6 +3,9 @@
 
 #include <cstdio>
 
+// MyTemplateClass takes three template parameters: X, Y, and Z.
+// It has a member function foo that takes a reference to a Y and returns an X.
+// It also has a private member variable that is a pointer to Z.
 template<typename X, typename Y, typename Z>
 struct MyTemplateClass {
     X foo(Y&);
@@ -14,3 +17,9 @@ int main() {
     // To instantiate a template class, use the following syntax:
     MyTemplateClass<int, double, char> my_concrete_class{};
 }
+
+// Notes:
+// The template keyword begins the template prefix, which specifies that the following declaration is a template.
+// The angle brackets <> enclose the template parameters, which are placeholders for types that will be specified when the template is instantiated.
+// The template parameters can be used in the class definition as if they were actual types. For example, in the foo function, Y is used as the type of the parameter, and X is used as the return type.
+// The member variable member is a pointer to Z, which means that when the template is instantiated, it will be a pointer to the type specified for Z.
