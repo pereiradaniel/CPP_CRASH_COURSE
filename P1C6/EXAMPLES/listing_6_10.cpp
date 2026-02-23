721ccd0a6010850aefefca9363bb2bd0e7bf28a2
Daniel Pereira <danielpaulpereira@protonmail.com>
2026-02-23 10:43:35 -0500

    Added notes about template instantiation regarding listing 6-10.

--- P1C6/EXAMPLES/listing_6_10.cpp
+++ P1C6/EXAMPLES/listing_6_10.cpp
@@ -1,7 +1,9 @@
 // Listing 6-10: Refactoring Listing 6-8 into a template function.
 #include <cstddef>
 
+// Template prefix with a single template parameter T.
 template<typename T>
+// Update mean to use T instead of double.
 T mean(const T* values, size_t length) {
     T result{};
     for(size_t i = 0; i < length; ++i) {
