7a19289ef700bd0f90ef65759493d1b8dd5321c9
Daniel Pereira <danielpaulpereira@protonmail.com>
2026-02-24 15:59:20 -0500

    Added notation to listing 6-18 and its valgrind output.

--- P1C6/EXAMPLES/listing_6_18.cpp
+++ P1C6/EXAMPLES/listing_6_18.cpp
@@ -1,12 +1,17 @@
-﻿#include <cstdint>
+﻿// Listing 6-18: A program using type traits.
+
+#include <cstdint>
 #include <cstdio>
 #include <type_traits>
 
+// A helper function to convert a boolean value to a string for printing:
 constexpr const char* as_str(bool x) {
+  // Return a string literal "True" if x is true, and "False" if x is false. This allows us to print the results of type trait checks in a more human-readable format:
   return x ? "True" : "False";
 }
 
 int main() {
+  // Use the is_integral type trait to check if various types are integral types, and print the results:
   printf("%s\n", as_str(std::is_integral<int>::value));
   printf("%s\n", as_str(std::is_integral<const int>::value));
   printf("%s\n", as_str(std::is_integral<char>::value));
