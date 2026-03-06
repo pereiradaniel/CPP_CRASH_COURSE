0bd98034444f75137b2fd03730bcbd4a43d8a4c7
Daniel Pereira <danielpaulpereira@protonmail.com>
2026-03-06 11:20:47 -0500

    Updated 6-4 to use sizeof instead of hardocded values.

--- P1C6/EXERCISES/6_4.cpp
+++ P1C6/EXERCISES/6_4.cpp
@@ -17,16 +17,16 @@ T mean(const T(&values)[Length]) {
 int main() {
   const double nums_d[]{ 1.0, 2.0, 3.0, 4.0 };
   // Usage makes it clear that template parameter T is double, so we can omit the explicit template argument.
-  const auto result1 = mean<double, 4>(nums_d);
+  const auto result1 = mean<double, sizeof(nums_d) / sizeof(double)>(nums_d);
   printf("double: %.1f\n", result1);
 
   const float nums_f[]{ 1.0f, 2.0f, 3.0f, 4.0f };
   // Usage makes it clear that template parameter T is float, so we can omit the explicit template argument.
-  const auto result2 = mean<float, 4>(nums_f);
+  const auto result2 = mean<float, sizeof(nums_f) / sizeof(float)>(nums_f);
   printf("float: %.1f\n", result2);
 
   const size_t nums_c[]{ 1, 2, 3, 4 };
   // Usage makes it clear that template parameter T is size_t, so we can omit the explicit template argument.
-  const auto result3 = mean<size_t, 4>(nums_c);
+  const auto result3 = mean<size_t, sizeof(nums_c) / sizeof(size_t)>(nums_c);
   printf("size_t: %lu\n", result3);
 }
