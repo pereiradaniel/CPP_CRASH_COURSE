60c677f81440697cdf194d4bf8b9780be4b91533
Daniel Pereira <danielpaulpereira@protonmail.com>
2026-03-18 10:51:06 -0400

    Added 6_6 valgrind output.

--- P1C6/EXERCISES/6_7.cpp
+++ P1C6/EXERCISES/6_7.cpp
@@ -1,3 +1,3 @@
 // Listing 6-7
 
-// Make Account an interface. Implement a CheckingAccount and SavingsAccount. Create a program with several checking and savings accountrs. Use a Bank<Account> to make several transactions between the accounts.
+// Make Account an interface. Implement a CheckingAccount and SavingsAccount. Create a program with several checking and savings accounts. Use a Bank<Account> to make several transactions between the accounts.
